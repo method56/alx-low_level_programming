929b8753a5090697155696c09a7869b06d6ed1ac
method56 <adjeiakporh56@gmail.com>
2023-02-17 14:31:45 -0800

    assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-        int n;
-        int L;
-        char str[] = "Last digit of";
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        L = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-        if (n % 10 > 5)
-                printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-        else if (n % 10 == 0)
-                printf("Last digit of %d is %d and is 0\n", n, n % 10);
-        else
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-        if (L > 5)
-                printf("%s %d is %d and is greater than 5\n", str, n, L);
-        else if (L == 0)
-                printf("%s %d is %d and is 0\n", str, n, L);
-        else if (L < 6)
-                printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-        return (0);
+	return (0);
 }
